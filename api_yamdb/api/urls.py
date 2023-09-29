7e957b74025b43c1c3464b01c11623780ee0af42
VitaliiCheremisov <vetal.cheremisov@gmail.com>
2023-09-29 11:33:03 +0300

    Написал весь раздел авторизации, тесты проходят)

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,16 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import SignUp, Token, UsersViewSet
+from .views import APISignUp, APIToken, UsersViewSet
 
 router_v1 = SimpleRouter()
 router_v1.register('users', UsersViewSet, basename='users')
 
 auth_patterns = [
-    path('signup/', SignUp.as_view(), name='signup'),
-    path('token/', Token.as_view(), name='token')
+    path('signup/', APISignUp.as_view(), name='signup'),
+    path('token/', APIToken.as_view(), name='token')
 ]
 
 urlpatterns = [
-    path('v1/auth/', include(auth_patterns))
+    path('v1/auth/', include(auth_patterns)),
+    path('v1/', include(router_v1.urls))
 ]
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,20 +1,21 @@
+import os
 from pathlib import Path
 
 from datetime import timedelta
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-# Application definition
-
 AUTH_USER_MODEL = 'reviews.CustomUser'
+
+PAGE_PAGINATION_NUMBER = 5
+
+TOKEN_LIFE_TIME_DAYS = 1
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -42,6 +43,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'api_yamdb.urls'
 
 TEMPLATES_DIR = BASE_DIR / 'templates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -60,18 +62,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
-
-# Database
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -88,9 +84,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -101,23 +94,25 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+
+EMAIL_PROJECT = 'vitalii.cheremisov@mail.ru'
+
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": PAGE_PAGINATION_NUMBER,
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=TOKEN_LIFE_TIME_DAYS),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
+
